f593dbf264071623c2cbc4c29a44d4b2bb152fc4
thalouchyk <tatsiana.halouchyk@pstlabs.by>
2020-01-14 12:14:15 +0300

    feat(app-component): component updating wit h database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dobrij-elektrik';
+export class AppComponent implements OnDestroy {
+  user: any[];
+
+  private destroy$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(db: AngularFireDatabase) {
+    db.list('/user')
+      .valueChanges()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(user => {
+        this.user = user;
+        console.log('user:', user);
+      });
+  }
+
+  public ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
